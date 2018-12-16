cf9a481379d1293854910648a8202944a20f6c0b
denism <denis@metrailler.ch>
2018-12-16 15:54:43 +0100

    add

--- src/app/models/outing.model.ts
+++ src/app/models/outing.model.ts
@@ -8,6 +8,11 @@ export class Outing {
   geojson: string;
   dangers: string[];
   pt_connections: string[];
+  name: string;
+  elevation_min: number;
+  elevation_max: number;
+  height_diff_up: number;
+
   constructor() {
   }
 }
